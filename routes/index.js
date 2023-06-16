47f5ae0193451ba99ed4d1138433b79928a5f475
Vyom Gupta <vyom.gupta2020@vitstudent.ac.in>
2023-06-16 23:41:32 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 const {loggedIn} = require('../middlewares/auth')
 
 router.get('/', (req, res) => {
+   res.header('Access-Control-Allow-Origin', 'https://summer-frontend.vercel.app')
    res.send('Hello World')
 })
 
 router.get('/dashboard', loggedIn, (req, res) => {
+   res.header('Access-Control-Allow-Origin', 'https://summer-frontend.vercel.app')
    res.send('dashboard')
 })
 
